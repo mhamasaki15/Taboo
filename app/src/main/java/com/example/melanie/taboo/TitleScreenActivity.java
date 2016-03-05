8cc0ab64950ced25ad3cd5841538fd382f0abcbd
Tony Ramos <ramosaj@usc.edu>
2016-03-05 15:21:15 -0800

    Made win activity

--- app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
+++ app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
@@ -4,10 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.Button;
-=======
->>>>>>> origin/master
+
 
 public class TitleScreenActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/melanie/taboo/WinnerScreenActivity.java
+++ app/src/main/java/com/example/melanie/taboo/WinnerScreenActivity.java
@@ -1,15 +1,36 @@
 package com.example.melanie.taboo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class WinnerScreenActivity extends Activity {
 
     @Override
+    public int score1;
+    public int score2;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner_screen);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView winner = (TextView) findViewById(R.id.textView5);
+        Intent win = getIntent();
+        score1 = win.getIntExtra(InGameActivity.T1_SCORE);
+        score2 = win.getIntExtra(InGameActivity.T2_SCORE);
+        if (score1 > score2) {
+            winner.setText("Team 1 wins!");
+        } else if (score2 > score1)
+        {
+            winner.setText("Team 2 wins!");
+        }
+        else
+        {
+            winner.setText("Tie game!");
+            //maybe include coin flip?
+        }
+
     }
 
 }
