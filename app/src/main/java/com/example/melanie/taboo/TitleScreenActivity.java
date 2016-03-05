305d1396d9dd40626564dbc4019db49e487e2681
Stacey Irawan <sirawan@usc.edu>
2016-03-05 14:00:31 -0800

    changes to startroundscreen
    -changes to startroundscreen and play button

--- app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
+++ app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 >>>>>>> origin/master
 
 public class TitleScreenActivity extends AppCompatActivity {
+    /*public String NEXT_UP = "0";
+    public String T1_SCORE = "0";
+    public String T2_SCORE = "0";
+    public String ROUNDS_LEFT= "0";*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,24 @@ public class TitleScreenActivity extends AppCompatActivity {
 
     public void sendtoAbout(View view){
         Intent intent = new Intent(this, AboutPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendtoRules(View view){
+        Intent intent = new Intent(this, RulesPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void sendtoStartRoundScreen(View view){
+        Intent intent = new Intent(this, StartRoundScreen.class);
+        /*int roundsLeft= 0;
+        intent.putExtra(ROUNDS_LEFT, roundsLeft);
+        int team = 1;
+        intent.putExtra(NEXT_UP, team);
+        int team1Score = 0;
+        intent.putExtra(T1_SCORE, team1Score);
+        int team2Score = 0;
+        intent.putExtra(T2_SCORE, team2Score);*/
         startActivity(intent);
     }
 }
