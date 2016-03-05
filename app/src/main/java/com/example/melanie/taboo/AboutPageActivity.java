367bdcb5693d4dfdb96a9c49545933c868c207eb
Stacey Irawan <sirawan@usc.edu>
2016-03-05 12:36:23 -0800

    create about intent
    created about intent and functions in java

---
+++ app/src/main/java/com/example/melanie/taboo/AboutPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.melanie.taboo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AboutPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+
+}
--- app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
+++ app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.melanie.taboo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TitleScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+    }
+
+    public void sendtoAbout(View view){
+        Intent intent = new Intent(this, AboutPageActivity.class);
+        startActivity(intent);
     }
 }
