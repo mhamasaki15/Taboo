148e945fa8defc9c7532dfca46710b75d592285c
Stacey Irawan <sirawan@usc.edu>
2016-03-05 15:10:26 -0800

    startroundscreen
    -got variables to work for startroundscreen

--- app/src/main/java/com/example/melanie/taboo/StartRoundScreen.java
+++ app/src/main/java/com/example/melanie/taboo/StartRoundScreen.java
@@ -8,27 +8,32 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class StartRoundScreen extends Activity {
-    public int ROUNDS_LEFT;
+/*    public int ROUNDS_LEFT;
     public int NEXT_UP;
     public int T1_SCORE;
     public int T2_SCORE;
+ */
+    private int sam;
+    private int sam2;
+    private int sam3;
+    private int sam4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         setContentView(R.layout.activity_start_round_screen);
-        ROUNDS_LEFT = intent.getIntExtra(TitleScreenActivity.ROUNDS_LEFT);
-        NEXT_UP = intent.getIntExtra( TitleScreenActivity.NEXT_UP);
-        T1_SCORE = intent.getIntExtra( TitleScreenActivity.T1_SCORE);
-        T2_SCORE = intent.getIntExtra( TitleScreenActivity.T2_SCORE);
+        sam = intent.getIntExtra(TitleScreenActivity.ROUNDS_LEFT, 21);
+        sam2 = intent.getIntExtra( TitleScreenActivity.NEXT_UP, 22);
+        sam3 = intent.getIntExtra( TitleScreenActivity.T1_SCORE, 23);
+        sam4 = intent.getIntExtra( TitleScreenActivity.T2_SCORE, 24);
         TextView next = (TextView) findViewById(R.id.textView);
         TextView left = (TextView) findViewById(R.id.textView2);
         TextView score1 = (TextView) findViewById(R.id.textView3);
         TextView score2 = (TextView) findViewById(R.id.textView4);
-        next.setText("Next up: " + NEXT_UP);
-        left.setText("Rounds Left: " + ROUNDS_LEFT);
-        score1.setText("Team 1: " + T1_SCORE);
-        score2.setText("Team 2: " + T2_SCORE);
+        next.setText("Next up: " + sam2);
+        left.setText("Rounds Left: " + sam);
+        score1.setText("Team 1: " + sam3);
+        score2.setText("Team 2: " + sam4);
 
 
 
@@ -38,7 +43,7 @@ public class StartRoundScreen extends Activity {
 
     }
 
-    public void RoundScreen(View view) {
+    /*public void RoundScreen(View view) {
         Intent send = new Intent(this, InGameActivity.class);
         send.putExtra("T1_SCORE", T1_SCORE);
         send.putExtra("T2_SCORE",T2_SCORE);
@@ -54,6 +59,6 @@ public class StartRoundScreen extends Activity {
 
 
     }
-
+*/
 
 }
--- app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
+++ app/src/main/java/com/example/melanie/taboo/TitleScreenActivity.java
@@ -4,16 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
+
 import android.widget.Button;
-=======
->>>>>>> origin/master
+
 
 public class TitleScreenActivity extends AppCompatActivity {
-    /*public String NEXT_UP = "0";
-    public String T1_SCORE = "0";
-    public String T2_SCORE = "0";
-    public String ROUNDS_LEFT= "0";*/
+    public static final String NEXT_UP = "0";
+    public static final String T1_SCORE = "1";
+    public static final String T2_SCORE = "2";
+    public static final String ROUNDS_LEFT= "6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +32,14 @@ public class TitleScreenActivity extends AppCompatActivity {
 
     public void sendtoStartRoundScreen(View view){
         Intent intent = new Intent(this, StartRoundScreen.class);
-        /*int roundsLeft= 0;
+        int roundsLeft= 6;
         intent.putExtra(ROUNDS_LEFT, roundsLeft);
         int team = 1;
         intent.putExtra(NEXT_UP, team);
         int team1Score = 0;
         intent.putExtra(T1_SCORE, team1Score);
         int team2Score = 0;
-        intent.putExtra(T2_SCORE, team2Score);*/
+        intent.putExtra(T2_SCORE, team2Score);
         startActivity(intent);
     }
 }
