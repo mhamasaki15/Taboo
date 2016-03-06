e1844e7ec4393c3a89120fae1e794f5d7ec403f4
Melanie Hamasaki <mhamasak@usc.edu>
2016-03-06 10:48:29 -0800

    here u go stae bae

---
+++ app/src/main/java/com/example/melanie/taboo/ExpansionPacks.java
@@ -0,0 +1,13 @@
+package com.example.melanie.taboo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExpansionPacks extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expansion_packs);
+    }
+}
