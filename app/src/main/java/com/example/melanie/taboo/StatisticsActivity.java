3e250cffb69948cb4d5b83547555db0c62fb1434
Melanie Hamasaki <mhamasak@usc.edu>
2016-03-05 20:55:49 -0800

    Statistics modifications

--- app/src/main/java/com/example/melanie/taboo/StatisticsActivity.java
+++ app/src/main/java/com/example/melanie/taboo/StatisticsActivity.java
@@ -3,14 +3,17 @@ package com.example.melanie.taboo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StatisticsActivity extends Activity {
     private int score1;
     private int score2;
 
-    private double shortestTime;
-    private String shortestTimeWord;
-    private int shortestTimeTeam;
+    private double sw_time, lw_time;
+    private String sw_word, lw_word;
+    private int sw_team, lw_team, hsr_score, hsr_team, hsr_round;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,14 @@ public class StatisticsActivity extends Activity {
 
 
          */
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stats_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 }
