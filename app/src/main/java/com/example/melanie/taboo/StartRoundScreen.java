bc183cdef7f7807c9be2b66b57226cab0f7afc99
Tony Ramos <ramosaj@usc.edu>
2016-03-05 19:39:23 -0800

    help me stacey is the sk

--- app/src/main/java/com/example/melanie/taboo/StartRoundScreen.java
+++ app/src/main/java/com/example/melanie/taboo/StartRoundScreen.java
@@ -1,6 +1,7 @@
 package com.example.melanie.taboo;
 
 import android.content.Intent;
+import android.media.TimedText;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -17,6 +18,16 @@ public class StartRoundScreen extends Activity {
     private int nextTeam;
     private int team1score;
     private int team2score;
+    private  String SW_WORD = "3";
+    private  int SW_TEAM;
+    private  double SW_TIME;
+    private  String LW_WORD = "11";
+    private  int LW_TEAM;
+    private  double LW_TIME;
+    private  int HSR_SCORE;
+    private  int HSR_TEAM;
+    private  int HSR_ROUND;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +36,16 @@ public class StartRoundScreen extends Activity {
         rounds = intent.getIntExtra(TitleScreenActivity.ROUNDS_LEFT, 0);
         nextTeam = intent.getIntExtra( TitleScreenActivity.NEXT_UP, 1);
         team1score = intent.getIntExtra( TitleScreenActivity.T1_SCORE, 0);
-        team2score = intent.getIntExtra( TitleScreenActivity.T2_SCORE, 0);
+        team2score = intent.getIntExtra(TitleScreenActivity.T2_SCORE, 0);
+        SW_WORD = intent.getStringExtra(TitleScreenActivity.SW_WORD);
+        SW_TIME = intent.getDoubleExtra(TitleScreenActivity.SW_TIME, 0.0);
+        SW_TEAM = intent.getIntExtra(TitleScreenActivity.SW_TEAM, 0);
+        LW_WORD = intent.getStringExtra(TitleScreenActivity.LW_WORD);
+        LW_TIME = intent.getDoubleExtra(TitleScreenActivity.LW_TIME, 0.0);
+        LW_TEAM = intent.getIntExtra(TitleScreenActivity.LW_TEAM, 0);
+        HSR_SCORE = intent.getIntExtra(TitleScreenActivity.HSR_SCORE,0);
+        HSR_TEAM = intent.getIntExtra(TitleScreenActivity.HSR_TEAM,0);
+        HSR_ROUND = intent.getIntExtra(TitleScreenActivity.HSR_ROUND,0);
         TextView next = (TextView) findViewById(R.id.next   );
         TextView left = (TextView) findViewById(R.id.rounds);
         TextView score1 = (TextView) findViewById(R.id.score1);
@@ -45,11 +65,15 @@ public class StartRoundScreen extends Activity {
         send.putExtra(TitleScreenActivity.T2_SCORE, team2score);
         send.putExtra(TitleScreenActivity.ROUNDS_LEFT, rounds);
         send.putExtra(TitleScreenActivity.NEXT_UP, nextTeam);
-
-        send.putExtra("T1_SCORE", team1score);
-        send.putExtra("T2_SCORE", team2score);
-        send.putExtra("ROUNDS_LEFT", rounds);
-        send.putExtra("NEXT_UP",nextTeam);
+        send.putExtra(TitleScreenActivity.SW_WORD, SW_WORD);
+        send.putExtra(TitleScreenActivity.SW_TIME, SW_TIME);
+        send.putExtra(TitleScreenActivity.SW_TEAM, SW_TEAM);
+        send.putExtra(TitleScreenActivity.LW_WORD, LW_WORD);
+        send.putExtra(TitleScreenActivity.LW_TIME,LW_TIME);
+        send.putExtra(TitleScreenActivity.LW_TEAM,LW_TEAM);
+        send.putExtra(TitleScreenActivity.HSR_SCORE,HSR_SCORE);
+        send.putExtra(TitleScreenActivity.HSR_TEAM,HSR_TEAM);
+        send.putExtra(TitleScreenActivity.HSR_ROUND,HSR_ROUND);
 
         startActivity(send);
 
